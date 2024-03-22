30c9f73405ae3f0b52fc6812bc67541a79b17350
Vukan <vjankovic6421rn@raf.rs>
2024-03-22 20:36:19 +0100

    cart izgled

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,6 +22,7 @@ import { HomeComponent } from "./pages/home/home.component";
 import { ProductsHeaderComponent } from "./pages/components/products-header/products-header.component";
 import { FiltersComponent } from "./pages/components/filters/filters.component";
 import { ProductBoxComponent } from './pages/components/product-box/product-box.component';
+import { CartComponent } from './pages/cart/cart.component';
 
 @NgModule({
   declarations: [
@@ -31,6 +32,7 @@ import { ProductBoxComponent } from './pages/components/product-box/product-box.
     ProductsHeaderComponent,
     FiltersComponent,
     ProductBoxComponent,
+    CartComponent,
   ],
   imports: [
     BrowserModule,
