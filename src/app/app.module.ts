808a1b9081860658e39a891da960efbceb410263
Vukan <vjankovic6421rn@raf.rs>
2024-03-22 17:32:44 +0100

    homepage gotov look

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,13 +17,21 @@ import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatTableModule } from "@angular/material/table";
 import { MatBadgeModule } from "@angular/material/badge";
 import { MatSnackBarModule } from "@angular/material/snack-bar";
-import { HeaderComponent } from './components/header/header.component';
-import { HomeComponent } from './pages/home/home.component';
-import { ProductsHeaderComponent } from './pages/components/products-header/products-header.component';
-import { FiltersComponent } from './pages/components/filters/filters.component';
+import { HeaderComponent } from "./components/header/header.component";
+import { HomeComponent } from "./pages/home/home.component";
+import { ProductsHeaderComponent } from "./pages/components/products-header/products-header.component";
+import { FiltersComponent } from "./pages/components/filters/filters.component";
+import { ProductBoxComponent } from './pages/components/product-box/product-box.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, HomeComponent, ProductsHeaderComponent, FiltersComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    ProductsHeaderComponent,
+    FiltersComponent,
+    ProductBoxComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
